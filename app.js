7cad66521c74ee995fa7f2981f6de7acf638fd4e
Ashish <ashishgiri.ingenio@gmail.com>
2021-10-01 15:49:13 +0530

    all

---
+++ app.js
@@ -0,0 +1,23 @@
+// ----------
+// Guess The Number
+// 1) Computer will generate a number between 1 to 50
+// 2) User can enter a number between 1 to 50
+// 3) If the user enters a higher or lower number, show a message
+// 4) Current Score will start with 50
+// 5) Each wrong guess will subtract 5 Points and shown in the current score
+// 6) Last Highest score will be always shown
+// 7) If the current score is 0, show a message "Oop!! You lose the game, Reset to try again."
+// 8) Reset will restart the game from any point
+// 9) Highest score will not be reset
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,43 @@
+const computer = Math.floor(Math.random() * 50);
+
+
+
+
+const check = document.querySelector('#ch');
+
+const guessN = document.querySelector('#Guess');
+
+const reset = document.querySelector('#re');
+
+const currsc = document.querySelector('#CR');
+const Hisc = document.querySelector('#HI')
+
+check.addEventListener('click', function () {
+    const userInput = document.querySelector('#one').value;
+    if (userInput == computer){
+        guessN.innerHTML= `You Guessed Correct,Your number is ${computer}`;
+        
+    
+    }else if (userInput < computer) {
+        guessN.innerHTML = "You Guess Too Lower!"
+
+    }
+       if (userInput > computer) {
+        guessN.innerHTML = "You Guess Too Higher!"
+
+    }
+   
+});
+
+
+reset.addEventListener('click' , function(userInput){
+userInput.preventDefault();
+})
+
+
+const cs= 50;
+currsc.innerHTML =`Current Score: ${cs}`
+
+
+
+
